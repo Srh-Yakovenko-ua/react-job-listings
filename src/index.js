260bba61a32bec3a9ac76dcd9a5f25dd3e76570e
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-11-27 23:48:09 +0300

    added redux-persist

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import './index.css';
 import App from './App';
 import {createRoot} from 'react-dom/client';
 import {Provider} from 'react-redux';
-import {store} from './store';
-
+import {persistor, store} from './store';
+import {PersistGate} from 'redux-persist/integration/react'
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(
     <Provider store={store}>
-        <App/>
+        <PersistGate loading={null} persistor={persistor}>
+            <App/>
+        </PersistGate>
     </Provider>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,22 @@
 import {legacy_createStore} from 'redux';
 import {rootReducer} from './rootReducer';
+import {persistStore, persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 const REDUX_DEVTOOLS = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['filter', 'positions'],
+    // blacklist : ['positions'],
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+
 export const store = legacy_createStore(
-    rootReducer,
+    persistedReducer,
     REDUX_DEVTOOLS
 )
+
+export const persistor = persistStore(store)
