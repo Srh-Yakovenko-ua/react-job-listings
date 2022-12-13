4889a2ac11c26fceae4ece6cf0cc78e8a7d68bec
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-12-13 22:38:58 +0300

    refactor rtk

---
+++ src/store.js
@@ -0,0 +1,41 @@
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {filterReducer} from './features/filter/filter-slice';
+import {positionsReducer} from './features/positions/positions-slice';
+import storage from 'redux-persist/lib/storage'
+import {
+    persistReducer,
+    persistStore,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
+import {logger} from 'redux-logger/src';
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const rootReducer = combineReducers({
+    filters: filterReducer,
+    positions: positionsReducer,
+})
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    devTools: true,
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }).concat(logger)
+    }
+})
+
+export const persistor = persistStore(store)
