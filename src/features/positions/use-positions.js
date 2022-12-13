6e2f82cface878ea93745fd0ef6b1f798e1aed07
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-12-13 22:38:43 +0300

    add custom hook

---
+++ src/features/positions/use-positions.js
@@ -0,0 +1,9 @@
+import {useSelector} from 'react-redux';
+import {selectVisiblePositions} from './positions-slice';
+import {selectFilter} from '../filter/filter-slice';
+
+export const usePositions = () => {
+    const currentFilters = useSelector(selectFilter)
+
+    return useSelector((state) => selectVisiblePositions(state, currentFilters))
+}
